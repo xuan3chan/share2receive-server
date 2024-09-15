b2a25bf4e812a9d7bba13937a7bfe615cb8d6b5a
xuan3chan <xuannguyen04072003@gmail.com>
2024-09-15 21:13:51 +0700

    ft

---
+++ src/admin/admin.controller.ts
@@ -0,0 +1,11 @@
+import { Controller } from '@nestjs/common';
+import { AdminService } from './admin.service';
+
+@Controller('admin')
+export class AdminController {
+  constructor(private readonly adminService: AdminService) {}
+  async getAdmin() {
+    console.log('getAdmin');
+    
+  }
+}
---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AdminModule } from './admin/admin.module';
+
+
+@Module({
+  imports: [AdminModule],
+  
+})
+export class AppModule {}

04198855dbbb56a09df88f1850a2da654cbbe430
xuan3chan <xuannguyen04072003@gmail.com>
2024-10-03 17:31:43 +0700

    Refactor brand and category modules to include AdminModule and AbilityFactory

--- src/brand/brand.module.ts
+++ src/brand/brand.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { BrandService } from './brand.service';
 import { BrandController } from './brand.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Brand, BrandSchema } from '@app/libs/common/schema';
+import { Admin, Brand, BrandSchema } from '@app/libs/common/schema';
+import { AbilityFactory } from '@app/libs/common/abilities';
+import { AdminModule } from 'src/admin/admin.module';
 
 @Module({
   imports:[
+    AdminModule,
     MongooseModule.forFeature([{ name: Brand.name, schema: BrandSchema }]),
   ],
   controllers: [BrandController],
-  providers: [BrandService],
+  providers: [BrandService,AbilityFactory],
 })
 export class BrandModule {}
--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -3,12 +3,16 @@ import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Category, CategorySchema } from '@app/libs/common/schema/category.schema';
+import { AbilityFactory } from '@app/libs/common/abilities';
+import { AdminModule } from 'src/admin/admin.module';
 
 @Module({
   imports: [
+    AdminModule
+,
     MongooseModule.forFeature([{ name: Category.name, schema: CategorySchema }]),
   ],
   controllers: [CategoryController],
-  providers: [CategoryService],
+  providers: [CategoryService,AbilityFactory],
 })
 export class CategoryModule {}

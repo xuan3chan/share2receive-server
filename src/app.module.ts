2162a1553e8843cb94623c1b2aa060e43ab0159b
xuan3chan <xuannguyen04072003@gmail.com>
2024-10-17 22:53:45 +0700

    Refactor app.module.ts to add Redis password configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -40,6 +40,7 @@ import { BullModule } from '@nestjs/bull';
       redis: {
         host: process.env.REDIS_HOST,
         port: parseInt(process.env.REDIS_PORT),
+        password: process.env.REDIS_PASSWORD, // Add this line
       },
     }),
   ],

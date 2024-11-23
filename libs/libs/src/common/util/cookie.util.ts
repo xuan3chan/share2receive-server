96394856be3e078495a6975e130dfc03aa8e82e9
xuan3chan <xuannguyen04072003@gmail.com>
2024-11-23 23:57:37 +0700

    Refactor cookie utility: remove optional domain parameter from setCookie and clearCookie functions

--- libs/libs/src/common/util/cookie.util.ts
+++ libs/libs/src/common/util/cookie.util.ts
@@ -17,7 +17,6 @@ export function setCookie(
     maxAge: 60 * 60 * 1000, // Default 1 hour
     sameSite: 'none',
     path: '/',
-    domain: undefined, // Optional: set a specific domain if required
   },
 ) {
   response.cookie(name, value, {
@@ -26,7 +25,6 @@ export function setCookie(
     maxAge: options.maxAge ?? 60 * 60 * 1000,
     sameSite: options.sameSite ?? 'none',
     path: options.path ?? '/',
-    domain: options.domain ?? undefined,
   });
 }
 
@@ -38,12 +36,10 @@ export function clearCookie(
     domain?: string;
   } = {
     path: '/', // Mặc định xóa cookie từ root
-    domain: undefined, // Mặc định không yêu cầu domain cụ thể
   },
 ) {
   response.clearCookie(name, {
     path: options.path ?? '/', // Đảm bảo path giống với khi cookie được tạo
-    domain: options.domain ?? undefined, // Đảm bảo domain giống với khi cookie được tạo
     secure: true, // Nếu cookie được tạo với secure=true, cần đảm bảo secure trong xóa cookie
     sameSite: 'none', // Giống khi tạo cookie
   });

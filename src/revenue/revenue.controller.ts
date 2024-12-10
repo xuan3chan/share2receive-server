33ac2630a72f13692210576edeb14c1a79c14e37
xuan3chan <xuannguyen04072003@gmail.com>
2024-12-10 11:31:12 +0700

    Add readRevenue permission and implement permission guard in revenue controller

--- src/revenue/revenue.controller.ts
+++ src/revenue/revenue.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { RevenueService } from './revenue.service';
 import { ApiQuery, ApiTags } from '@nestjs/swagger';
+import { PermissionGuard } from '@app/libs/common/gaurd';
+import { Action, Subject } from '@app/libs/common/decorator';
 
 @ApiTags('revenue')
 @Controller('revenue')
 export class RevenueController {
   constructor(private readonly revenueService: RevenueService) {}
 
+  
   @Get('get-revenue-For-manager')
+  @UseGuards(PermissionGuard)
+  @Subject('revenue')
+  @Action('read')
   @ApiQuery({ name: 'page', required: false })
   @ApiQuery({ name: 'limit', required: false })
   @ApiQuery({ name: 'filterBy', required: false })

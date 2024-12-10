2c42c48097219f62a27c19171bb03503c075243b
xuan3chan <xuannguyen04072003@gmail.com>
2024-12-10 11:38:02 +0700

    Add AdminModule and AbilityFactory to RevenueModule for enhanced permissions management

--- src/revenue/revenue.module.ts
+++ src/revenue/revenue.module.ts
@@ -3,14 +3,17 @@ import { RevenueService } from './revenue.service';
 import { RevenueController } from './revenue.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Revenue, RevenueSchema } from '@app/libs/common/schema';
+import { AdminModule } from 'src/admin/admin.module';
+import { AbilityFactory } from '@app/libs/common/abilities';
 
 @Module({
   imports:[
+    AdminModule,
  MongooseModule.forFeature([
    { name: Revenue.name, schema: RevenueSchema },
   ]),],
   controllers: [RevenueController],
-  providers: [RevenueService],
+  providers: [RevenueService,AbilityFactory],
   exports: [RevenueService],
 })
 export class RevenueModule {}
